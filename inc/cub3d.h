4d686d17357fddccf29850634fc178da4bbe951c
riel-fas <riadelfassi17@gmail.com>
2025-10-16 13:15:16 +0100

    fixed norm

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,13 +6,12 @@
 /*   By: riel-fas <riel-fas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 02:36:35 by riel-fas          #+#    #+#             */
-/*   Updated: 2025/10/16 12:53:19 by riel-fas         ###   ########.fr       */
+/*   Updated: 2025/10/16 13:14:59 by riel-fas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
- 
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
@@ -70,12 +69,12 @@
 /* Texture identifiers */
 typedef enum e_texture_type
 {
-	NO = 0,	// North texture
-	SO = 1,	// South texture
-	WE = 2,	// West texture
-	EA = 3,	// East texture
-	F = 4,	// Floor color
-	C = 5	// Ceiling color
+	NO = 0,
+	SO = 1,
+	WE = 2,
+	EA = 3,
+	F = 4,
+	C = 5
 }	t_texture_type;
 
 /* Color structure */
@@ -84,7 +83,7 @@ typedef struct s_color
 	int	r;
 	int	g;
 	int	b;
-	int	hex;		// Hex representation for MLX
+	int	hex;
 }	t_color;
 
 /* Vector structure for raycasting */
@@ -97,22 +96,22 @@ typedef struct s_vector
 /* Ray structure */
 typedef struct s_ray
 {
-	t_vector	pos;		// Ray position
-	t_vector	dir;		// Ray direction
-	t_vector	delta_dist;	// Distance between x/y intersections
-	t_vector	side_dist;	// Distance to next x/y intersection
-	t_vector	step;		// Step direction (-1 or 1)
-	int			map_x;		// Current map position
+	t_vector	pos;
+	t_vector	dir;
+	t_vector	delta_dist;	
+	t_vector	side_dist;
+	t_vector	step;
+	int			map_x;
 	int			map_y;
-	int			hit;		// Wall hit flag
-	int			side;		// Which side was hit (0=x, 1=y)
-	double		wall_dist;	// Distance to wall
-	int			line_height;// Height of wall line to draw
-	int			draw_start;	// Start pixel of wall
-	int			draw_end;	// End pixel of wall
-	int			tex_num;	// Texture number (0-3)
-	double		wall_x;		// Exact wall hit position
-	int			tex_x;		// Texture x coordinate
+	int			hit;
+	int			side;
+	double		wall_dist;
+	int			line_height;
+	int			draw_start;
+	int			draw_end;
+	int			tex_num;
+	double		wall_x;
+	int			tex_x;
 }	t_ray;
 
 /* Texture structure */
@@ -121,7 +120,7 @@ typedef struct s_texture
 	mlx_texture_t	*mlx_texture;
 	int				width;
 	int				height;
-	uint32_t		**pixels;	// 2D array of pixel data
+	uint32_t		**pixels;
 }	t_texture;
 
 /* Key state structure */
@@ -137,211 +136,191 @@ typedef struct s_keys
 
 typedef struct s_data
 {
-	/* MLX */
 	mlx_t		*mlx;
 	mlx_image_t	*image;
-	
-	/* Textures */
-	char		*texture_paths[4];	// NO, SO, WE, EA
-	t_texture	textures[4];		// Loaded texture data
+	char		*texture_paths[4];
+	t_texture	textures[4];
 	t_color		floor_color;
 	t_color		ceiling_color;
-	
-	/* Map */
 	char		**map;
 	int			map_width;
 	int			map_height;
-	
-	/* Player */
 	double		player_x;
 	double		player_y;
-	char		player_dir;		// N, S, E, W
-	double		player_angle;	// Angle in radians
-	
-	/* Player vectors for 3D engine */
-	t_vector	player_pos;		// Player position as vector
-	t_vector	player_dir_vec;	// Player direction vector
-	t_vector	camera_plane;	// Camera plane for FOV
-	
-	/* Game state */
+	char		player_dir;
+	double		player_angle;
+	t_vector	player_pos;
+	t_vector	player_dir_vec;
+	t_vector	camera_plane;
 	int			game_running;
 	t_keys		keys;
-	
-	/* Parsing flags */
-	int			textures_parsed[4];	// individual texture flags
-	int			colors_parsed[2];	// floor and ceiling flags
+	int			textures_parsed[4];
+	int			colors_parsed[2];
 	int			map_started;
 	int			parsing_complete;
-	
-	/* File data */
 	char		**file_lines;
 	int			line_count;
 	char		*filename;
-	
-	/* Validation data */
 	int			empty_lines_in_map;
-	int			**visited;		// For flood fill algorithm
+	int			**visited;
 }	t_data;
 
-
-int	count_words_internal(char *str, char delimiter);
-
-
 /* Function prototypes */
-
 /* Main */
-int		main(int argc, char **argv);
-int		validate_args(int argc, char **argv);
+int			main(int argc, char **argv);
+int			validate_args(int argc, char **argv);
 
 /* Parsing */
-int		parse_file(t_data *data, char *filename);
-int		parse_textures_and_colors(t_data *data);
-int		parse_map(t_data *data);
-int		validate_map(t_data *data);
+int			parse_file(t_data *data, char *filename);
+int			parse_textures_and_colors(t_data *data);
+int			parse_map(t_data *data);
+int			validate_map(t_data *data);
+int			count_words_internal(char *str, char delimiter);
 
 /* File content parsing */
-int		iterate_through_lines(t_data *data);
-int		process_lines_until_map(t_data *data);
-int		is_empty_or_comment(char *line);
-int		process_single_line(t_data *data, char *trimmed_line);
-int		validate_parsing_completeness(t_data *data);
-int		handle_line_processing_result(int result, char *trimmed_line);
-int		process_single_line_with_cleanup(t_data *data, char *trimmed_line);
-char	*get_trimmed_line(char **file_lines, int index);
+int			iterate_through_lines(t_data *data);
+int			process_lines_until_map(t_data *data);
+int			is_empty_or_comment(char *line);
+int			process_single_line(t_data *data, char *trimmed_line);
+int			validate_parsing_completeness(t_data *data);
+int			handle_line_processing_result(int result, char *trimmed_line);
+int			process_single_line_with_cleanup(t_data *data, char *trimmed_line);
+char		*get_trimmed_line(char **file_lines, int index);
 
 /* Texture parsing */
-int		get_texture_type(char *line);
-int		validate_texture_extension(char *path);
-int		parse_texture_line(t_data *data, char *line);
+int			get_texture_type(char *line);
+int			validate_texture_extension(char *path);
+int			parse_texture_line(t_data *data, char *line);
 
 /* RGB/Color parsing */
-int		parse_rgb_values(char *rgb_str, t_color *color);
-int		parse_color_line(t_data *data, char *line);
+int			parse_rgb_values(char *rgb_str, t_color *color);
+int			parse_color_line(t_data *data, char *line);
 
 /* Map parsing */
-int		process_map_line_index(t_data *data, int file_index, int *map_index);
-int		populate_map_data(t_data *data, int map_start);
-int		is_config_line(char *trimmed_line);
-int		should_skip_line(char *trimmed_line);
-int		find_map_start(t_data *data);
-int		count_map_lines(t_data *data, int start);
-int		get_map_width(t_data *data, int start);
-int		initialize_map_data(t_data *data, int *map_start);
-int		allocate_map_memory(t_data *data);
-int		add_line_to_map(t_data *data, char *trimmed_line, int map_index);
+int			process_map_line_index(t_data *data,
+				int file_index, int *map_index);
+int			populate_map_data(t_data *data, int map_start);
+int			is_config_line(char *trimmed_line);
+int			should_skip_line(char *trimmed_line);
+int			find_map_start(t_data *data);
+int			count_map_lines(t_data *data, int start);
+int			get_map_width(t_data *data, int start);
+int			initialize_map_data(t_data *data, int *map_start);
+int			allocate_map_memory(t_data *data);
+int			add_line_to_map(t_data *data, char *trimmed_line, int map_index);
 
 /* Parsing utils */
-int		is_texture_line(char *line);
-int		is_color_line(char *line);
-int		is_map_line(char *line);
-int		parse_texture_path(t_data *data, char *line);
-int		parse_color(t_data *data, char *line);
-int		skip_whitespace(char *line, int *index);
-int		get_texture_index(char *line);
+int			is_texture_line(char *line);
+int			is_color_line(char *line);
+int			is_map_line(char *line);
+int			parse_texture_path(t_data *data, char *line);
+int			parse_color(t_data *data, char *line);
+int			skip_whitespace(char *line, int *index);
+int			get_texture_index(char *line);
 
 /* Map validation */
-int		is_valid_map_char(char c);
-int		check_walls(t_data *data);
-int		find_player(t_data *data);
-int		flood_fill_check(t_data *data);
-int		validate_map_format(t_data *data);
+int			is_valid_map_char(char c);
+int			check_walls(t_data *data);
+int			find_player(t_data *data);
+int			flood_fill_check(t_data *data);
+int			validate_map_format(t_data *data);
 
 /* Flood fill validation */
-void	flood_fill(char **map_copy, int x, int y, t_data *data);
-int		validate_flood_fill_result(t_data *data, char **map_copy);
-int		flood_fill_validation(t_data *data);
+void		flood_fill(char **map_copy, int x, int y, t_data *data);
+int			validate_flood_fill_result(t_data *data, char **map_copy);
+int			flood_fill_validation(t_data *data);
 
 /* Player validation */
-int		is_player_char(char c);
-int		is_valid_char(char c);
-int		count_and_set_player(t_data *data);
-int		find_player_position(t_data *data);
-int		validate_characters(t_data *data);
+int			is_player_char(char c);
+int			is_valid_char(char c);
+int			count_and_set_player(t_data *data);
+int			find_player_position(t_data *data);
+int			validate_characters(t_data *data);
 
 /* Border validation */
-int		check_horizontal_borders(t_data *data);
-int		check_vertical_borders(t_data *data);
-int		check_map_borders(t_data *data);
-int		is_space_connecting_to_border(t_data *data, int x, int y);
-int		check_empty_spaces_near_borders(t_data *data);
+int			check_horizontal_borders(t_data *data);
+int			check_vertical_borders(t_data *data);
+int			check_map_borders(t_data *data);
+int			is_space_connecting_to_border(t_data *data, int x, int y);
+int			check_empty_spaces_near_borders(t_data *data);
 
 /* Wall validation */
-int		is_surrounded_by_walls(t_data *data, int x, int y);
-int		validate_walls(t_data *data);
+int			is_surrounded_by_walls(t_data *data, int x, int y);
+int			validate_walls(t_data *data);
 
 /* Advanced validation */
-int		check_empty_lines_in_map(t_data *data);
-int		validate_file_extension(char *filename);
-int		validate_texture_files(t_data *data);
-int		check_duplicate_configs(t_data *data);
+int			check_empty_lines_in_map(t_data *data);
+int			validate_file_extension(char *filename);
+int			validate_texture_files(t_data *data);
+int			check_duplicate_configs(t_data *data);
 
 /* Reference-style validation */
-int		validate_map_boundaries(t_data *data);
-int		check_directions_ver(t_data *data, int x, int y);
-int		check_directions_hor(t_data *data, int x, int y);
-int		check_line(t_data *data, int y);
+int			validate_map_boundaries(t_data *data);
+int			check_directions_ver(t_data *data, int x, int y);
+int			check_directions_hor(t_data *data, int x, int y);
+int			check_line(t_data *data, int y);
 
 /* Utils */
-int		ft_strlen(char *str);
-char	*ft_strdup(char *str);
-char	**ft_split(char *str, char delimiter);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strtrim(char *str);
-int		ft_isdigit(char c);
-int		ft_atoi(char *str);
-int		ft_isspace(char c);
-char	*ft_strchr(char *str, char c);
-int		count_words(char *str, char delimiter);
-char	*ft_strncpy(char *dest, const char *src, int n);
+int			ft_strlen(char *str);
+char		*ft_strdup(char *str);
+char		**ft_split(char *str, char delimiter);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+char		*ft_strtrim(char *str);
+int			ft_isdigit(char c);
+int			ft_atoi(char *str);
+int			ft_isspace(char c);
+char		*ft_strchr(char *str, char c);
+int			count_words(char *str, char delimiter);
+char		*ft_strncpy(char *dest, const char *src, int n);
 
 /* Error handling */
-void	error_exit(char *message);
-void	free_data(t_data *data);
-void	free_split(char **split);
+void		error_exit(char *message);
+void		free_data(t_data *data);
+void		free_split(char **split);
 
 /* Additional map helper functions */
-char	*pad_map_line(char *line, int target_width);
-char	*process_map_line(char *file_line);
+char		*pad_map_line(char *line, int target_width);
+char		*process_map_line(char *file_line);
 
 /* Engine functions */
-int		init_game(t_data *data);
-int		init_mlx(t_data *data);
-int		init_player(t_data *data);
-void	setup_player_vectors(t_data *data);
-int		load_textures(t_data *data);
-void	cleanup_textures(t_data *data);
+int			init_game(t_data *data);
+int			init_mlx(t_data *data);
+int			init_player(t_data *data);
+void		setup_player_vectors(t_data *data);
+int			load_textures(t_data *data);
+void		cleanup_textures(t_data *data);
 uint32_t	get_pixel_color(t_texture *texture, int x, int y);
 uint32_t	rgb_to_hex(int r, int g, int b);
 
 /* Raycasting */
-void	cast_rays(t_data *data);
-void	init_ray(t_data *data, t_ray *ray, int x);
-void	calculate_step_and_side_dist(t_data *data, t_ray *ray);
-void	perform_dda(t_data *data, t_ray *ray);
-void	calculate_wall_distance(t_ray *ray);
-void	calculate_draw_bounds(t_ray *ray);
-void	cast_single_ray(t_data *data, int x);
+void		cast_rays(t_data *data);
+void		init_ray(t_data *data, t_ray *ray, int x);
+void		calculate_step_and_side_dist(t_data *data, t_ray *ray);
+void		perform_dda(t_data *data, t_ray *ray);
+void		calculate_wall_distance(t_ray *ray);
+void		calculate_draw_bounds(t_ray *ray);
+void		cast_single_ray(t_data *data, int x);
 
 /* Rendering */
-void	render_frame(t_data *data);
-void	draw_vertical_line(t_data *data, int x, t_ray *ray);
-void	draw_textured_wall(t_data *data, int x, t_ray *ray);
-void	draw_floor_and_ceiling(t_data *data, int x, t_ray *ray);
+void		render_frame(t_data *data);
+void		draw_vertical_line(t_data *data, int x, t_ray *ray);
+void		draw_textured_wall(t_data *data, int x, t_ray *ray);
+void		draw_floor_and_ceiling(t_data *data, int x, t_ray *ray);
 uint32_t	get_texture_pixel(t_data *data, t_ray *ray, int y);
 
 /* Input handling */
-void	handle_input(mlx_key_data_t keydata, void *param);
-void	handle_mouse(double xpos, double ypos, void *param);
-void	process_movement(t_data *data);
-void	move_player(t_data *data, double move_x, double move_y);
-void	rotate_player(t_data *data, double angle);
+void		handle_input(mlx_key_data_t keydata, void *param);
+void		handle_mouse(double xpos, double ypos, void *param);
+void		process_movement(t_data *data);
+void		move_player(t_data *data, double move_x, double move_y);
+void		rotate_player(t_data *data, double angle);
 
 /* Game loop */
-void	game_loop(void *param);
-void	update_game(t_data *data);
+void		game_loop(void *param);
+void		update_game(t_data *data);
 
 /* Math utilities */
-double	degrees_to_radians(double degrees);
-double	normalize_angle(double angle);
+double		degrees_to_radians(double degrees);
+double		normalize_angle(double angle);
 
 #endif
